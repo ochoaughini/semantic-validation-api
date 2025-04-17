cfb2054890886c90d22f553810714cb217c54d95
ochoaughini <augustoochoa.augustoochoa@gmail.com>
2025-04-17 16:21:30 -0300

    Update model.py

--- backend/nano backend/nano backend/model.py
+++ backend/nano backend/nano backend/model.py
@@ -1,10 +1,1 @@
-from sentence_transformers import SentenceTransformer
 
-# Carregamento único do modelo de embeddings semânticos
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def embed_text(text: str):
-    """
-    Gera o embedding para a sentença fornecida.
-    """
-    return model.encode(text)
